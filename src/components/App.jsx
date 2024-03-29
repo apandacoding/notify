e1eaae50624f0a675120a6a75894db54060a15fa
Aryan Panda <aryanpanda2004@gmail.com>
2024-03-29 18:55:16 -0400

    ready to push

---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  function editNote(id, updatedNote) {
+    setNotes(prevNotes => {
+      return prevNotes.map((noteItem, index) => {
+        if (index === id) {
+          return {
+            ...noteItem,
+            ...updatedNote
+          };
+        }
+        return noteItem;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+            onEdit={editNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Aryan's React Project</p>
+    </footer>
+  );
+}
+
+export default Footer;
